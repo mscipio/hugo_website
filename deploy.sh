2142f50ee9e3d39abaa34940b7ab0660c9e85359
Michele Scipioni <mscipio@scipioni.workgroup>
2017-11-14 16:01:02 +0100

    rebuilding site Tue Nov 14 16:01:02 CET 2017

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t academic # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo #-t academic # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+rm ./.git/index.lock
 
 # Go To Public folder
 cd public
